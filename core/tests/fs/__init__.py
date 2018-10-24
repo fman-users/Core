c39166fa343e0aed4b16da17925b2907614cd256
Michael Herrmann <michael@herrmann.io>
2018-10-24 12:29:29 +0200

    Source code of the Core plugin in fman 1.3.7

---
+++ core/tests/fs/__init__.py
@@ -0,0 +1,32 @@
+from fman.fs import FileSystem, cached
+from fman.impl.util import filenotfounderror
+from fman.impl.util.path import resolve
+
+class StubFileSystem(FileSystem):
+
+	scheme = 'stub://'
+
+	def __init__(self, items):
+		super().__init__()
+		self._items = items
+	def exists(self, path):
+		return resolve(path) in self._items
+	@cached # Mirror a typical implementation
+	def is_dir(self, existing_path):
+		path_resolved = resolve(existing_path)
+		try:
+			item = self._items[path_resolved]
+		except KeyError:
+			raise filenotfounderror(existing_path)
+		return item.get('is_dir', False)
+	@cached # Mirror a typical implementation
+	def size_bytes(self, path):
+		return self._items[resolve(path)].get('size', 1)
+	@cached # Mirror a typical implementation
+	def modified_datetime(self, path):
+		return self._items[resolve(path)].get('mtime', 1473339041.0)
+	def touch(self, path):
+		file_existed = self.exists(path)
+		self._items[resolve(path)] = {}
+		if not file_existed:
+			self.notify_file_added(path)
