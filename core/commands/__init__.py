dcd9c4cb35ba26babbf6e476c0271c9a78064a8a
Michael Herrmann <michael@herrmann.io>
2018-11-27 11:27:05 +0100

    Source code of the Core plugin in fman 1.4.7

--- core/commands/__init__.py
+++ core/commands/__init__.py
@@ -80,7 +80,6 @@ class MoveCursorPageDown(DirectoryPaneCommand):
 class ToggleSelection(DirectoryPaneCommand):
 	def __call__(self):
 		file_under_cursor = self.pane.get_file_under_cursor()
-		self.pane.deselect(file_under_cursor)
 		if file_under_cursor:
 			self.pane.toggle_selection(file_under_cursor)
 
@@ -845,12 +844,13 @@ class CopyPathsToClipboard(DirectoryPaneCommand):
 
 def _report_clipboard_action(verb, files, suffix='', ftype='file'):
 	num = len(files)
+	first_file = as_human_readable(files[0])
 	if num == 1:
-		message = '%s "%s"%s.' % (verb, files[0], suffix)
+		message = '%s %s%s' % (verb, first_file, suffix)
 	else:
 		plural = 's' if num > 2 else ''
-		message = '%s "%s" and %d other %s%s%s.' % \
-				  (verb, files[0], num - 1, ftype, plural, suffix)
+		message = '%s %s and %d other %s%s%s' % \
+				  (verb, first_file, num - 1, ftype, plural, suffix)
 	show_status_message(message, timeout_secs=3)
 
 class CopyToClipboard(DirectoryPaneCommand):
